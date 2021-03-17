54c43bb87bb60488cc97f0dfedd0b17d5ae6eb43
Aislin Bamber <aislin.bamber@gmail.com>
2021-03-17 11:23:01 +0000

    mobile responsive edits

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ function About() {
         <h3>Junior Software Engineer</h3>
       </div>
       <div className="about-me">
-          I am a linguist and analyst pursuing a career in software engineering. I&apos;ve worked in tech-adjacent roles within sales and business operations, from start-ups to international corporations. As a fresh graduate from General Assembly&apos;s Software Engineering Immersive course, I&apos;m now looking for a junior dev role in a stimulating environment.
+          I am a linguist and analyst now pursuing a career in software engineering. I&apos;ve worked in tech-adjacent roles within sales and business operations, from start-ups to international corporations. Newly graduated from General Assembly&apos;s Software Engineering Immersive course, I&apos;m now looking for a junior dev role in a stimulating environment.
       </div>
     </section>
   )
